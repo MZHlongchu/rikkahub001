4eaacbda3c48721239857e8f25c317b804634469
yuxinjiang218-creator <yuxinjiang218@gmail.com>
2026-02-08 20:20:53 +0800

    fix: 修复 upper/usr/local 目录未创建的 bug
    之前代码: parentFile?.mkdirs() 只创建父目录
    修复后: mkdirs() 创建完整目录路径

--- app/src/main/java/me/rerere/rikkahub/data/container/PRootManager.kt
+++ app/src/main/java/me/rerere/rikkahub/data/container/PRootManager.kt
@@ -529,7 +529,7 @@ class PRootManager(private val context: Context) {
         val upperDir = File(containerDir, "upper").apply { mkdirs() }
         
         // 创建 bind mount 所需的子目录
-        File(upperDir, "usr/local").apply { parentFile?.mkdirs() }
+        File(upperDir, "usr/local").apply { mkdirs() }
         File(upperDir, "root").apply { mkdirs() }
 
         globalContainer = ContainerState(
